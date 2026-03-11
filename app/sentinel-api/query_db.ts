c02bfc7778ad8ddfac488bdb29a724ae1afc64fe
Mark Joseph Livado <livadomc@gmail.com>
2026-03-11 16:18:52 +0800

    chore: Remove various temporary test and utility files.

--- app/sentinel-api/query_db.ts
+++
@@ -1,9 +0,0 @@
-import { db } from '@sentinel/db';
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-async function main() {
-    const res = await db.selectFrom('auth.users').select(['id', 'email', 'role', 'raw_user_meta_data']).limit(5).execute();
-    console.log(JSON.stringify(res, null, 2));
-}
-main();
