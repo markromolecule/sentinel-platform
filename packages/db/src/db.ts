63571ccb969fa563fed0e70f95f00dc83c3e818d
Mark Joseph Livado <livadomc@gmail.com>
2026-03-23 17:21:51 +0800

    fixing accelerate prisma

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,48 +1,28 @@
 import { withAccelerate } from '@prisma/extension-accelerate';
-import {
-    PrismaClient,
-    Prisma,
-    type users as PrismaUser,
-    type user_profiles as PrismaUserProfile,
-} from '../generated/client';
-export { Prisma };
-export type { PrismaUser, PrismaUserProfile };
+import { PrismaClient } from '../generated/client';
 import { Kysely, PostgresAdapter, PostgresIntrospector, PostgresQueryCompiler } from 'kysely';
 import kyselyExtension from 'prisma-extension-kysely';
 import type { DB } from './generated/types';
 
 const createClient = () => {
-    const databaseUrl = process.env.DATABASE_URL;
-    const directUrl = process.env.DIRECT_URL;
-    const connectionUrl = databaseUrl || directUrl;
-
-    const options: any = {
+    return new PrismaClient({
         log: ['error', 'warn'],
-    };
-
-    if (connectionUrl?.startsWith('prisma://')) {
-        options.accelerateUrl = connectionUrl;
-    } else if (connectionUrl) {
-        options.datasources = {
-            db: {
-                url: connectionUrl,
-            },
-        };
-    }
-
-    return new PrismaClient(options).$extends(withAccelerate()).$extends(
-        kyselyExtension<DB>({
-            kysely: (driver) =>
-                new Kysely<DB>({
-                    dialect: {
-                        createDriver: () => driver,
-                        createAdapter: () => new PostgresAdapter(),
-                        createIntrospector: (db) => new PostgresIntrospector(db),
-                        createQueryCompiler: () => new PostgresQueryCompiler(),
-                    },
-                }),
-        }),
-    );
+        accelerateUrl: process.env.DATABASE_URL,
+    })
+        .$extends(withAccelerate())
+        .$extends(
+            kyselyExtension<DB>({
+                kysely: (driver) =>
+                    new Kysely<DB>({
+                        dialect: {
+                            createDriver: () => driver,
+                            createAdapter: () => new PostgresAdapter(),
+                            createIntrospector: (db) => new PostgresIntrospector(db),
+                            createQueryCompiler: () => new PostgresQueryCompiler(),
+                        },
+                    }),
+            }),
+        );
 };
 
 export type PrismaKyselyClient = ReturnType<typeof createClient>;
